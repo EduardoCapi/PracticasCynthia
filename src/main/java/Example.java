5aca8e100eb39640cf9b6e1fab8879b70f0eb0f8
EduardoCapi <eduardo.capistran@uabc.edu.mx>
2023-02-24 17:38:55 -0800

    Cambios 1

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,19 +1,29 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Example {
-    public static void main(String[] args) {
-        ChromeDriver driver;
-        String baseURL = "https://demo.guru99.com/selenium/newtours/";
-        String actualResult = "";
-        String expectedResult= "Welcome: Mercury Tours";
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+    ChromeDriver driver;
+    String baseURL = "https://demo.guru99.com/selenium/newtours/";
+    String actualResult = "";
+    String expectedResult= "Welcome: Mercury Tours";
 
+    @BeforeClass
+    public void declaracionVariables(){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void prueba1() {
         driver.get(baseURL);
         actualResult = driver.getTitle();
+    }
 
+    @AfterClass
+    public void configuraciónFinal(){
         System.out.println(actualResult.equals(expectedResult)?"PASSED: " + actualResult : "FALLED: " + actualResult);
-
         driver.quit();
     }
 }
